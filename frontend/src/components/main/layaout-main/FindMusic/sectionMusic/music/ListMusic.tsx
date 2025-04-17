5b75f9013813009ff96b8be2c275561b3bc8edea
M-Azariel <walter.azariel.moreno@gmail.com>
2025-04-17 19:14:43 -0300

    maquetado el responsive, los titulos, y preparando un contexto

--- frontend/src/components/main/layaout-main/FindMusic/sectionMusic/music/ListMusic.tsx
+++ frontend/src/components/main/layaout-main/FindMusic/sectionMusic/music/ListMusic.tsx
@@ -30,6 +30,11 @@ export const ListMusic = () => {
                 </h3>
             </div>
             <div className='w-full h-full flex-center bg-primary'>
+                <div className='text-tertiary bg-primary w-full h-20 flex justify-between items-center font-uniq text-lg lg:text-3xl ml-12 p-10'>
+                    <h2 className='lg:ml-9 xl:ml-20 2xl:ml-32'>Track</h2>
+                    <h2 className='lg:mr-5'>Artist</h2>
+                    <h2 className='xl:mr-14 2xl:mr-24'>Genre</h2>
+                </div>
                 {playMusic.map((file) => (
                     <div key={file.id} className='bg-primary h-20 w-full'>
                         <MusicPlayer 
